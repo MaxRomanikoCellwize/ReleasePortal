a00c3a2a35c3df41b85705d1ad7e242fbed4f579
Max Romaniko <max.romaniko@cellwize.com>
2018-01-17 15:31:12 +0200

    Index html is displayed

--- ReleaseBot/releasebot/views.py
+++ ReleaseBot/releasebot/views.py
@@ -4,7 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+
 # Create your views here.
 def index(request):
-    output = ''
-    return HttpResponse(output)
+    template = loader.get_template('releasebot/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
