632ce25205124ff9de9791a2dacbb725a4f02ea5
Max Romaniko <max.romaniko@cellwize.com>
2018-01-17 15:28:30 +0200

    Rename folder

--- ReleaseBot/releasebot/apps.py
+++ ReleaseBot/releasebot/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class ReleaseportalConfig(AppConfig):
-    name = 'ReleasePortal'
+class releasebotConfig(AppConfig):
+    name = 'releasebot'
--- ReleaseBot/releasebot/views.py
+++ ReleaseBot/releasebot/views.py
@@ -6,4 +6,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    output = ''
+    return HttpResponse(output)
