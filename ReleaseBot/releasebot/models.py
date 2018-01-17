28e9673ab50de9b49fa8cb0baca65f5beca763e5
asafk <asaf.katz@cellwize.com>
2018-01-17 20:25:15 +0200

    Created data model

--- ReleaseBot/releasebot/models.py
+++ ReleaseBot/releasebot/models.py
@@ -3,12 +3,39 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Products(models.Model):
+    name = models.CharField(max_length=200)
+    order = models.IntegerField(default=1)
+    jenkinsContainer = models.CharField(max_length=200)
 
+class Releases(models.Model):
+    name = models.CharField(max_length=200)
+    order = models.IntegerField(default=1)
+    type = models.CharField(max_length=50)
+    date = models.DateField('date released')
+    releaseNotes = models.CharField(max_length=200)
+    technicalNotes = models.CharField(max_length=200)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class ProductReleases(models.Model):
+    releaseId = models.ForeignKey(Releases)
+    productId = models.ForeignKey(Products)
+    jenkinsJobName = models.CharField(max_length=200)
+    buildNumber = models.IntegerField
+
+class ServicePacks(models.Model):
+    name = models.CharField(max_length=200)
+    order = models.IntegerField(default=1)
+    productReleaseId = models.ForeignKey(ProductReleases)
+    buildNumber = models.IntegerField
+    date = models.DateField('date installed')
+
+class Accounts(models.Model):
+    name = models.CharField(max_length=200)
+    order = models.IntegerField(default=1)
+
+class AccountReleases(models.Model):
+    accountId = models.ForeignKey(Accounts)
+    productReleaseId = models.ForeignKey(ProductReleases)
+    target = models.CharField(max_length=50)
+    date = models.DateField('date released')
+
